e47aa909f99772efc6be51393742c9fdcb059e17
butapig <x86@live.jp>
2013-10-01 21:31:25 +0900

    select double

--- sc.py
+++ sc.py
@@ -1,1 +1,23 @@
-print('Hello World')
+import bpy
+
+obj = bpy.context.active_object
+
+dic = {}
+list =[]
+
+if obj.type == 'MESH':
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.context.tool_settings.mesh_select_mode = (True, False, False)
+    i=0
+    for vt in obj.data.vertices:
+        x = str(vt.co)
+        if dic.get(x,0) == 1:
+            list.append(i)
+        dic[x] = 1
+        i+=1
+        
+bpy.ops.mesh.select_all(action = 'DESELECT')
+bpy.ops.object.mode_set(mode = 'OBJECT')
+for j in list:
+    obj.data.vertices[j].select = True   
+bpy.ops.object.mode_set(mode = 'EDIT')
